3e604f1257e0944f7b25cf0fc97d4648ad034ef9
Aimen Khakwani and Ayana Powell <aimen@>
2016-08-24 13:50:57 -0700

    Add js to toggle between players

--- js/scripts.js
+++ js/scripts.js
@@ -17,30 +17,54 @@ $(document).ready(function() {
   }
 
   Player.prototype.showScore = function() {
-    return "Hey, " + this.name + "! Your total is: " + this.score;
+    return "Your total is: " + this.score;
   }
+
+
 
 //ui logic
 var score = 0;
 var name = "Anna";
 var playerOne = new Player(name, score);
 var playerTwo = new Player(name, score);
+var holdScoreOne = [];
+var holdScoreTwo = [];
 
 // Player One Roll
+
 $('#rollOne').click(function() {
   var diceRoll =  Math.floor(Math.random() * 6 +1);
 
   if(diceRoll > 1) {
     playerOne.addScore(diceRoll);
-    $('.diceroll').text("Hey Player 1, you rolled a " + diceRoll);
-    $('.total').text(playerOne.showScore());
+    $('#output1 .diceroll').text("Hey Player 1, you rolled a " + diceRoll);
+    $('#output1 .total').text(playerOne.showScore());
     return;
   } else if (diceRoll === 1) {
     playerOne.resetScore(diceRoll);
-    $('.diceroll').text("Hey Player 1, you rolled a " + diceRoll);
-    $('.total').text(playerOne.showScore());
+    $('#output1 .diceroll').text("Hey Player 1, you rolled a " + diceRoll);
+    $('#output1 .total').text(playerOne.showScore());
+    $(".turn1").addClass('show');
+    $("#rollOne").prop('disabled', true);
+    $("#holdOne").prop('disabled', true);
+    $("#rollTwo").prop('disabled', false);
+    $("#holdTwo").prop('disabled', false);
     return;
   }
+});
+
+$('#holdOne').click(function() {
+  holdScoreOne.splice(0, 1, playerOne.score);
+  console.log(holdScoreOne);
+  $("#rollTwo").prop('disabled', false);
+  $("#holdTwo").prop('disabled', false);
+  $("#rollOne").prop('disabled', true);
+  $("#holdOne").prop('disabled', true);
+  // if (holdScoreOne === 100){
+  //   $("#output").text("<h1> Player One Wins!!!!</h1>");
+  //   $('.playerOne').fadeOut();
+  //   $('.player2').fadeOut();
+  // }
 });
 
 // Player Two Roll
@@ -49,14 +73,27 @@ $('#rollTwo').click(function() {
 
   if(diceRoll > 1) {
     playerTwo.addScore(diceRoll);
-    $('.diceroll').text("Hey Player 2, you rolled a " + diceRoll);
-    $('.total').text(playerTwo.showScore());
+    $('#output2 .diceroll').text("Hey Player 2, you rolled a " + diceRoll);
+    $('#output2 .total').text(playerTwo.showScore());
     return;
   } else if (diceRoll === 1) {
     playerTwo.resetScore(diceRoll);
-    $('.diceroll').text("Hey Player 2, you rolled a " + diceRoll);
-    $('.total').text(playerTwo.showScore());
+    $('#output2 .diceroll').text("Hey Player 2, you rolled a " + diceRoll);
+    $('#output2 .total').text(playerTwo.showScore());
+    $(".turn2").addClass('show');
+    $("#rollTwo").prop('disabled', true);
+    $("#holdTwo").prop('disabled', true);
+    $("#rollOne").prop('disabled', false);
+    $("#holdOne").prop('disabled', false);
     return;
   }
+});
+$('#holdTwo').click(function() {
+  holdScoreTwo.splice(0, 1, playerTwo.score);
+  console.log(holdScoreTwo);
+  $("#rollTwo").prop('disabled', true);
+  $("#holdTwo").prop('disabled', true);
+  $("#rollOne").prop('disabled', false);
+  $("#holdOne").prop('disabled', false);
 });
 });
